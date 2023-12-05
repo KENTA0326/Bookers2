a5bcecf3f4b36f0a92f83856ed4e1e6d03505d5a
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2023-12-05 11:31:06 +0000

    Initial Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
 
   def create #コメントを登録する
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.comment.new(comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to books_path
-  end 
-  
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    comment = Comment.find_by(id: params[:id])
+    comment.destroy
+  end
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:content)
   end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,16 @@
 class FavoritesController < ApplicationController
    def create
-    book = Book.find(params[:book_id])
-    #favorite = current_user.favorites.new(post_id: post.id)
-    #favorite.save
-    current_user.favorites.create(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
+    #current_user.favorites.create(book_id: book.id)
 
-    redirect_to book_path(book)
-  end
+   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to book_path(book)
+
   end
 end
